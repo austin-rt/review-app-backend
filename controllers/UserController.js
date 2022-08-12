cacab37bfa25bf9c9fa5b77508d92428d2126a5d
Deonte LaBorde <deonte.laborde@gmail.com>
2022-08-12 15:09:04 -0400

    create user routes and controllers

---
+++ controllers/UserController.js
@@ -0,0 +1,35 @@
+const { User, Review } = require('../models')
+
+const GetUsers = async (req, res) => {
+  try {
+    const users = await User.findAll()
+    res.send(users)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetUserById = async (req,res) => {
+  try {
+    const user = await User.findByPk(req.params.user_id)
+    res.send(user)
+  }catch(error) {
+    throw error
+  }
+} 
+
+const CreateUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+
+module.exports = {
+  GetUsers,
+  GetUserById,
+  CreateUser
+}
