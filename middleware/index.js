eb54303521120130777fb55b306d65bce48f3bb3
austin-rt <austintaylorart@gmail.com>
2022-08-15 11:03:25 -0400

    create stripToken function

--- middleware/index.js
+++ middleware/index.js
@@ -33,9 +33,22 @@ const verifyToken = (req, res, next) => {
   }
 };
 
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1];
+    if (token) {
+      res.locals.token = token;
+      return next();
+    }
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' });
+  }
+};
+
 module.exports = {
   hashPassword,
   comparePassword,
   createToken,
-  verifyToken
+  verifyToken,
+  stripToken
 };
