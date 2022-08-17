7dc2b46abd23fcdfb5753a1131b1d7abd9e09070
Deonte LaBorde <deonte.laborde@gmail.com>
2022-08-17 16:17:59 -0400

    added  instance of username to reviews

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,28 +1,34 @@
-const { Movie, Review } = require('../models');
+const { Movie, Review, User } = require('../models')
 
 const GetMovieDetails = async (req, res) => {
   try {
     const movie = await Movie.findByPk(req.params.movie_id, {
-      include: [{ model: Review, as: 'movie_reviews' }]
-    });
-    res.send(movie);
+      include: [
+        {
+          model: Review,
+          as: 'movie_reviews',
+          include: [{ model: User, as: 'user_reviews' }]
+        }
+      ]
+    })
+    res.send(movie)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const GetAllMovies = async (req, res) => {
   try {
     const movies = await Movie.findAll({
       include: [{ model: Review, as: 'movie_reviews' }]
-    });
-    res.send(movies);
+    })
+    res.send(movies)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 module.exports = {
   GetMovieDetails,
   GetAllMovies
-};
+}
