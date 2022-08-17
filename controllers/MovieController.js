94f8072eba23676f07c1d3962fbc2e854d001131
austin-rt <austintaylorart@gmail.com>
2022-08-17 16:27:33 -0400

    updated get all movie controller

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,4 +1,4 @@
-const { Movie, Review, User } = require('../models')
+const { Movie, Review, User } = require('../models');
 
 const GetMovieDetails = async (req, res) => {
   try {
@@ -10,25 +10,31 @@ const GetMovieDetails = async (req, res) => {
           include: [{ model: User, as: 'user_reviews' }]
         }
       ]
-    })
-    res.send(movie)
+    });
+    res.send(movie);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 const GetAllMovies = async (req, res) => {
   try {
     const movies = await Movie.findAll({
-      include: [{ model: Review, as: 'movie_reviews' }]
-    })
-    res.send(movies)
+      include: [
+        {
+          model: Review,
+          as: 'movie_reviews',
+          include: [{ model: User, as: 'user_reviews' }]
+        }
+      ]
+    });
+    res.send(movies);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 module.exports = {
   GetMovieDetails,
   GetAllMovies
-}
+};
