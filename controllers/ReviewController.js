6c1e2462aac9fe4ccdb67274e99d5927abf8abef
austin-rt <austintaylorart@gmail.com>
2022-08-17 19:16:25 -0400

    add delete review button

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -1,53 +1,54 @@
-const { Review } = require('../models')
+const { Review } = require('../models');
 
 const GetAllReviews = async (req, res) => {
   try {
-    const reviews = await Review.findAll({})
-    res.send(reviews)
+    const reviews = await Review.findAll({});
+    res.send(reviews);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
+
 const CreateReview = async (req, res) => {
   try {
-    let userId = parseInt(req.params.user_id)
-    let movieId = parseInt(req.params.movie_id)
-  
+    let userId = parseInt(req.params.user_id);
+    let movieId = parseInt(req.params.movie_id);
+
     let reviewBody = {
       userId,
       movieId,
       ...req.body
-    }
-    let review = await Review.create(reviewBody)
-    res.send(review)
+    };
+    let review = await Review.create(reviewBody);
+    res.send(review);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 const UpdateReview = async (req, res) => {
   try {
-    let reviewId = parseInt(req.params.review_id)
+    let reviewId = parseInt(req.params.review_id);
     let updatedReview = await Review.update(req.body, {
       where: { id: reviewId },
       returning: true
-    })
-    res.send(updatedReview)
+    });
+    res.send(updatedReview);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 const DeleteReview = async (req, res) => {
   try {
-    let reviewId = parseInt(req.params.review_id)
-    await Review.destroy({ where: { id: reviewId } })
-    res.send({ message: `Deleted review with an id of ${reviewId}` })
+    let reviewId = parseInt(req.params.review_id);
+    await Review.destroy({ where: { id: reviewId } });
+    res.send({ message: `Deleted review with an id of ${reviewId}` });
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 module.exports = {
   GetAllReviews,
   CreateReview,
   UpdateReview,
   DeleteReview
-}
+};
--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -1,25 +1,25 @@
-const Router = require('express').Router()
-const middleware = require('../middleware')
-const controller = require('../controllers/ReviewController')
+const Router = require('express').Router();
+const middleware = require('../middleware');
+const controller = require('../controllers/ReviewController');
 
-Router.get('/', controller.GetAllReviews)
+Router.get('/', controller.GetAllReviews);
 Router.post(
   '/:movie_id/:user_id',
   middleware.stripToken,
   middleware.verifyToken,
   controller.CreateReview
-)
+);
 Router.put(
   '/:review_id',
   middleware.stripToken,
   middleware.verifyToken,
   controller.UpdateReview
-)
+);
 Router.delete(
   '/:review_id',
   middleware.stripToken,
   middleware.verifyToken,
   controller.DeleteReview
-)
+);
 
-module.exports = Router
+module.exports = Router;
