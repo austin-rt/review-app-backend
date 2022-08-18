8ea200b20748f1d1021e23786ab6c5250862af52
Deonte LaBorde <deonte.laborde@gmail.com>
2022-08-18 12:08:02 -0400

    double quote config.js

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
 require('dotenv').config()
 module.exports = {
-  development: {
-    database: 'rated_our_development',
-    dialect: 'postgres'
+  "development": {
+    "database": "rated_our_development",
+    "dialect": "postgres"
   },
-  test: {
-    database: 'rated_our_test',
-    dialect: 'postgres'
+  "test": {
+    "database": "rated_our_test",
+    "dialect": "postgres"
   },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-        require: true
+  "production": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false,
+        "require": true
       }
     }
   }
